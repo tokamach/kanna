6c2a157c64218e77623a211ca4c7a805b0cb2307
Tom Hutchings <goerofmuns@gmail.com>
2017-04-16 14:04:50 +0100

    work begins

---
+++ gap_buffer.h
@@ -0,0 +1,9 @@
+// gap buffer data structure
+typedef struct 
+{
+  buf[]
+  char* pre;  //pre gap array
+  char* post; //post gap array
+
+  char
+} GapBuffer;
---
+++ vector.h
@@ -0,0 +1,15 @@
+#define VECTOR_INITIAL_CAPACITY 50
+
+typedef struct
+{
+  int size;
+  int capacity;
+  char* slots;
+} Vector;
+
+void vector_init(Vector *vector);
+void vector_append(Vector *vector, char val);
+void vector_get(Vector *vector, int pos);
+void vector_set(Vector *vector, int pos, char val);
+void vector_double_capacity_if_full(Vector *vector);
+void vector_free(Vector *vector);
