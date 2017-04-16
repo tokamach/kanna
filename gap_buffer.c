f741dde3d7542344b837add359b185d603ba563b
Tom Hutchings <goerofmuns@gmail.com>
2017-04-16 14:28:33 +0100

    work on gap buffer begins

---
+++ gap_buffer.c
@@ -0,0 +1,10 @@
+#include "gap_buffer.h"
+#include "vector.h"
+
+void gb_init(GapBuffer *gb)
+{
+  gb->pre = 0;
+  gb->post = 0;
+
+  vector_init(gb->buf);
+}
--- gap_buffer.h
+++ gap_buffer.h
@@ -1,9 +1,22 @@
+#pragma strict
+#include "vector.h"
+
 // gap buffer data structure
 typedef struct 
 {
-  buf[]
-  char* pre;  //pre gap array
-  char* post; //post gap array
+  Vector buf;
 
-  char
+  int size; //buffer size
+  int pre;  //pre gap array index
+  int post; //post gap array index
+
 } GapBuffer;
+
+void gb_init(GapBuffer *gb);
+
+void gb_step_forward(GapBuffer *gb);
+void gb_step_backward(GapBuffer *gb);
+void gb_move_by(GapBuffer *gb, int mov);
+
+void gb_insert_char(GapBuffer *gb, char val);
+void gb_insert_str(GapBuffer *gb, char *vals);
