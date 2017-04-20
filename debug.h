b5c6a39498446061dc55381138f776093bda68a2
Tom Hutchings <goerofmuns@gmail.com>
2017-04-20 16:25:33 +0100

    more debugging

--- debug.h
+++ debug.h
@@ -11,4 +11,5 @@ void debug_print(Editor *e)
   mvprintw(0, mx - 9, "xsize: %d", mx);
   mvprintw(1, mx - 8, "curx: %d", e->cur_x);
   mvprintw(2, mx - 8, "cury: %d", e->cur_y);
+  mvprintw(3, mx - 8, "prev: %d", e->prev_index);
 }
